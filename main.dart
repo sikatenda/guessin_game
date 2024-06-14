69177cea910ce4e3548b89916c5f3cc522d9d673
Jacques Feruzi <jferuzi019@gmail.com>
2024-06-14 17:15:56 +0200

    guess the number

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:math';
+
+main() {
+  Random random = new Random();
+
+  print('Enter your guessing number:');
+  var number = stdin.readLineSync();
+  var numberConverted = int.parse(number ?? '0');
+
+  for (int a = 1; a <= 3; a++) {
+    var rand = random.nextInt(5) + 1;
+    if (rand == numberConverted) {
+      print('Congatulations you guessed the right !');
+      print('The number guessed by the computer is $rand');
+      break;
+    } else {
+      print("Attempt $a failled!!!");
+      print('You guessed the wrong number !');
+      print('The number guessed by the computer is $rand');
+      if (a == 3) {
+        break;
+      }
+    }
+
+    print(" ");
+    print('Enter your guessing number once again: ');
+    number = stdin.readLineSync();
+  }
+}
